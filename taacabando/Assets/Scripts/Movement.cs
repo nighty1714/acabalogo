fb971e6caebbc4cf51ffeabbd3d5f1dbd602a91d
RafaelN9 <rafael.n.correia@unesp.br>
2021-11-06 16:29:44 -0300

    Player v0.1 funcionando

--- taacabando/Assets/Scripts/Movement.cs
+++ taacabando/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float velocity = 0.1f;
+    private bool isColiding = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,33 +15,39 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if(Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)){
-            gameObject.transform.position += gameObject.transform.forward*velocity;
+        if(!isColiding){
+            if(Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)){
+                gameObject.transform.position += gameObject.transform.forward*velocity;
+            }
+            if(Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)){
+                gameObject.transform.position += gameObject.transform.forward*-1*velocity;
+            }
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+                gameObject.transform.position += gameObject.transform.right*-1*velocity;
+            }
+            if(Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)){
+                gameObject.transform.position += gameObject.transform.right*velocity;
+            }
         }
-        if(Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)){
-            gameObject.transform.position += gameObject.transform.forward*-1*velocity;
-        }
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-            gameObject.transform.position += gameObject.transform.right*-1*velocity;
-        }
-        if(Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)){
-            gameObject.transform.position += gameObject.transform.right*velocity;
-        }
-
     }
     private void OnCollisionEnter(Collision collision){
-        if(Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)){
-            gameObject.transform.position += gameObject.transform.forward*-2f*velocity;
+        if(collision.gameObject.layer == 7){
+            isColiding = true;
         }
-        if(Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)){
-            gameObject.transform.position += gameObject.transform.forward*2f*velocity;
-        }
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-            gameObject.transform.position += gameObject.transform.right*2f*velocity;
-        }
-        if(Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)){
-            gameObject.transform.position += gameObject.transform.right*-2f*velocity;
-        }
+        // if(Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow)){
+        //     //gameObject.transform.position += gameObject.transform.forward*-2f*velocity;
+        // }
+        // if(Input.GetKey(KeyCode.S)  || Input.GetKey(KeyCode.DownArrow)){
+        //     //gameObject.transform.position += gameObject.transform.forward*2f*velocity;
+        // }
+        // if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+        //     //gameObject.transform.position += gameObject.transform.right*2f*velocity;
+        // }
+        // if(Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)){
+        //     //gameObject.transform.position += gameObject.transform.right*-2f*velocity;
+        // }
+    }
+    private void OnCollisionExit(Collision other) {
+        isColiding = false;
     }
 }
